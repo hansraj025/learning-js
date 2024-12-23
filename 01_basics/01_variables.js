2547a107881913cf0991aa2cbd5fa09f7ebd9986
Hansraj Singh Shekhawat <114150228+hansraj025@users.noreply.github.com>
2024-12-23 18:53:02 +0000

    let's constantly not use var.

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1;
+let accountEmail = "hansraj@gmail.com";
+var accountPassword = "12345678";
+accountCity = "Jaipur";
+let accountState;
+
+// accountId = 2; // Changing constants not allowed
+accountEmail = "hs@business.com";
+accountPassword = "87654321";
+accountCity = "Jodhpur";
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
