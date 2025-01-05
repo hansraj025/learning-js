a24163cde99010b178fc619a5ccd241774e0cde9
Hansraj Singh Shekhawat <114150228+hansraj025@users.noreply.github.com>
2025-01-05 14:16:31 +0000

    typeof undefined is undefined and null is an object (-_-)

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// alert(3 + 3) // We are using nodeJs not the browser
+
+
+let num = 5;            // number
+let studying = true;    // boolean
+let state = null;       // null => standalone value
+let left;               // undefined
+let greeting = 'Hello'; // String
+// Object
+
+console.log(typeof 5);
+console.log(typeof studying);
+
+
+console.log(typeof null);       // output is object (* ? *)
+
+
+console.log(typeof undefined);  // output is undefined
+
+
+console.log(typeof greeting);
