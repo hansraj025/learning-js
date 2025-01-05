128cca67ed6ba4d4ab6de623c71a718279e0f159
Hansraj Singh Shekhawat <114150228+hansraj025@users.noreply.github.com>
2025-01-05 18:51:40 +0000

    Avoid different data type comparisons. Clean Code (0v0)

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,9 +1,11 @@
+// ******************** Conversions **************************
 let score1 = '33';
 let score2 = '33abc';
 let score3 = null;
 let isStudying = true;
 
-console.log(typeof score1);
+// console.log(typeof score1);
+
 
 // Converting to Number
 let valueInNumber1 = Number(score1);
@@ -12,17 +14,50 @@ let valueInNumber3 = Number(score3);
 let isStudyingInNumber = Number(isStudying);
 
 // '33' => 33
-console.log(typeof valueInNumber1);
-console.log(valueInNumber1);
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1);
 
 // '33abc' => NaN(Not a Number)
-console.log(typeof valueInNumber2);
-console.log(valueInNumber2);
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2);
 
 // null => 0
-console.log(typeof valueInNumber3);
-console.log(valueInNumber3);
+// console.log(typeof valueInNumber3);
+// console.log(valueInNumber3);
 
 // true => 1; false => 0
-console.log(typeof isStudying);
-console.log(isStudyingInNumber);
+// console.log(typeof isStudying);
+// console.log(isStudyingInNumber);
+
+
+// ******************** Operations **************************
+// let value = 3;
+// let negValue = -value;
+// console.table([value, negValue]);
+
+
+// console.log('1' + 2);        // Outputs 12
+// console.log(2 + '1');        // Outputs 21
+// console.log('1' + 2 + 2);    // Outputs 122 (0_0)
+// console.log(2 + 2 + '1');    // Outputs 41  (0_0)
+
+
+// console.log(true);
+// console.log(+true);          // Outputs 1
+// console.log(+'');            // Outputs 0
+// console.log(true+);          // [Guess (*v*)]
+
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2 + 2; // There are better ways to write it
+// num1 = 1;
+// num2 = 2;
+// [num1, num2] = [num2, num1];
+// console.table([num1, num2]);    // num1 => 2 and num2 => 1
+
+
+// let videoNumber = 7;
+// console.log(videoNumber);   // 7
+// console.log(videoNumber++); // 7 => videoNumber incremented to 8 after printing
+// console.log(++videoNumber); // 9 => videoNumber incremented before printing
+
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log('2' > 1);
+// console.log('02' > 1);
+
+
+/* 
+Comparison operators [>, <], [==, ===] and [>= , <=]
+work differently. Avoid conversions between different
+data types. Clean Code.
+*/
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+
