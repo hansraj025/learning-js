e2d43c4f5ba7b8842068aaaa12759b4e1711a4b7
Hansraj Singh Shekhawat <114150228+hansraj025@users.noreply.github.com>
2025-01-06 12:15:24 +0000

    Added that primitives use stack and non primitives use heap.

--- 01_basics/04.1_dataTypesSummary.js
+++ 01_basics/04.1_dataTypesSummary.js
@@ -34,16 +34,39 @@ const bigNumber = 999999999999999999n;
 // Array, Objects, Functions
 
 const heroes = ['Isagi Yoichi', 'Old Man Lan', 'Blue Ranger'];
-console.log(typeof heroes);
+// console.log(typeof heroes);
 
 let myObj = {
     'name': 'Isagi Yoichi',
     'anime': 'Blue Lock'
 };
-console.log(typeof myObj);
+// console.log(typeof myObj);
 
 const myFunction = function() {
     console.log("Hello, World!");
 };
-console.log(typeof myFunction);
-myFunction();
+// console.log(typeof myFunction);
+// myFunction();
+
+
+// ****** Primitive => Stack, Non primitive => Heap ******
+// Primitive => Stack
+// String, Number, Symbol, 
+let playerName = 'Isagi Yoichi';
+let playerNumber = 11;
+let anotherPlayerNumber = playerNumber;
+anotherPlayerNumber = 10;   // Does not change playerNumber
+// console.log(playerNumber, anotherPlayerNumber); // pN => 10 and aPN => 9
+
+
+// Non Primitive (Reference) => Heap
+// Array, objects, functions
+let player = {
+    'name': 'Igaguri',
+    'rank': 300
+};
+
+let anotherPlayer = player; // Reference stored in player is copied anotherPlayer
+anotherPlayer.name = 'Isagi Yoichi';    // any modifications will be reflected in the original
+anotherPlayer.rank = 299;
+// console.log(player);
