d393bdc0ad22c293ddce08815261de196ac3a089
Hansraj Singh Shekhawat <114150228+hansraj025@users.noreply.github.com>
2025-01-06 15:14:20 +0000

    Explored strings.

---
+++ 05_strings.js
@@ -0,0 +1,63 @@
+let playerName = 'Hyouma Chigiri';
+let playerAlias = 'Princess';
+let playerNumber = 5;
+let playerProfileURL = 'https://www.bluelock.com/hyouma%20chigiri';
+
+// console.log(playerName + playerNumber); // Don't do this
+
+// console.log(`${playerName} jersey number is ${playerNumber}.`); // Do this
+
+// console.log(playerName);
+
+// console.log(playerName.__proto__);
+
+// console.log(playerName.anchor('1'));   // returns an anchor tag with attribute name='1'
+
+// console.log(playerName.at(7));  // returns playerName[7]
+
+// console.log(playerName.big());  // returns a big html element
+
+// console.log(playerName.blink());
+
+// console.log(playerName.bold());
+
+// console.log(playerName.charAt(5));
+
+// console.log(playerName.charCodeAt(5));  // utf char code of element at index 5
+
+// console.log(playerName.codePointAt(5)); // utf char code of element at index 5
+
+// console.log(playerName.concat(' runs very fast.'));
+
+let playerName2 = String('Igaguri Gurimu'); // Constructor for String
+// console.log(playerName2);
+
+// console.log(playerName.endsWith('x'));
+
+// console.log(playerName.fixed());
+
+// console.log(playerName.fontcolor('red'));
+
+// console.log(playerName.fontsize(16));
+
+// console.log(playerProfileURL.includes('Blue', 0));
+
+// console.log(playerName.indexOf('a'));
+
+// console.log(playerAlias.isWellFormed());    // returns true if there are no [lone surrogates]*
+
+// console.log(playerName.italics());
+
+// console.log(playerName2.lastIndexOf('g', 10));   // returns the last index of substring 'g' in playerName2, starts search from given index and decreasing
+
+// console.log(playerName.length);  // property of string
+
+// console.log(playerName.link(playerProfileURL)); // returns an anchor tag with href set to passed value
+
+console.log(playerName.localeCompare());
+
+
+
+
+
+let playerName3 = String('Isagi Yoichi');
